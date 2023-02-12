cdc45ec40f0259126ae92592f4e2a45b13d46f35
ПК <ruslano48@yandex.ru>
2023-02-12 08:53:37 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] agrs)
-    {
-System.out.println("Ruslan Ostapenko");
-        }
-        }
+    public static void main(String[] agrs) {
+        System.out.println("Ruslan Ostapenko");
+    }
+}
