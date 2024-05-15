c29a5cf5c0a5422fe91ac7ebf0b4a81329476add
user <ruslano48@yandex.ru>
2024-05-15 10:28:40 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] agrs) {
+       System.out.println("Ruslan Ostapenko");
+       System.out.println("05.04.1983");
+    }
+}
