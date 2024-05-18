b398d260a24e660d37b2099e6fcf84357f90007d
user <ruslano48@yandex.ru>
2024-05-18 10:54:09 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/ArgumentsMethod.java
+++ src/main/java/ru/job4j/ArgumentsMethod.java
@@ -5,7 +5,7 @@ public class ArgumentsMethod {
         System.out.println("Hello, " + name + ", age = " + age);
     }
 
-    public static void main(String[]  args) {
+    public static void main(String[] args) {
         String name = "Job4j";
         int age = 6;
         ArgumentsMethod.hello(name, age);
