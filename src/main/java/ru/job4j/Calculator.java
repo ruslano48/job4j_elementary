014f2d5fb1fa3d24f6b59e9222c0920a21666729
user <ruslano48@yandex.ru>
2024-05-18 10:01:08 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -21,20 +21,3 @@ public class Calculator {
     }
 
 }
-
-class ArgumentsMethod {
-
-    public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ", age = " + age);
-    }
-
-    public static void main(String[] args) {
-        String name = "Job4j";
-        int age = 6;
-        ArgumentsMethod.hello(name, age);
-        ArgumentsMethod.hello(name, age);
-        ArgumentsMethod.hello(name, age);
-        ArgumentsMethod.hello(name, age);
-    }
-}
-
