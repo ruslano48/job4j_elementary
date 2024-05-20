cd5e09e951b714ba224ad9e5de9f2377b5c2b5c9
user <ruslano48@yandex.ru>
2024-05-20 10:03:29 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+public class Point {
+    public static double distance (int x1, int x2, int y1, int y2) {
+        double a = x2 - x1;
+        double b = y2 - y1;
+        double c = Math.pow(a, 2);
+        double d = Math.pow(b, 2);
+        double f = c + d;
+        double g = Math.sqrt(f);
+        double result = g;
+        return result;
+
+    }
+
+
+    public static void main(String[] agrs) {
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0,0) to (2,0) " + result);
+
+    }
+}
+
+
+
