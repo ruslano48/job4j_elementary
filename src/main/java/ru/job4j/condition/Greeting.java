ec326ac30f3bd4ebdab9a0f51da2b59df65bcb3e
user <ruslano48@yandex.ru>
2024-05-21 20:06:46 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] agrs) {
+
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + "Вut I am a newbie";
+        int year = 2024;
+        String id = idea + year;
+        System.out.println(idea);
+
+    }
+}
