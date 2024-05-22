6bf9c675f2a6d151588ee810a509c64c0d750b18
user <ruslano48@yandex.ru>
2024-05-22 08:55:00 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -16,6 +16,6 @@ public class Converter {
         float euro = rubleToEuro(140);
         Float dollar = rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140rubles are " + dollar + "dollar");
+        System.out.println("140 rubles are " + dollar + "dollar");
     }
 }
