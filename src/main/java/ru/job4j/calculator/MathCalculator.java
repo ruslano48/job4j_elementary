7d8b07b9929c7d4eed8cf7d8c07a8c07403b6c0d
user <ruslano48@yandex.ru>
2024-05-25 07:49:02 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculator;
+
+import ru.job4j.math.MathFunction;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+}
