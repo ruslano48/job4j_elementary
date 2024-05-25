ce984f901bd1f1402d5156e6b4e1b6d2b56f4894
user <ruslano48@yandex.ru>
2024-05-25 21:05:18 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -4,13 +4,16 @@ public class MathFunction {
     public static double sum(double first, double second) {
         return first + second;
     }
+
     public static double multiply(double first, double second) {
         return first * second;
     }
-    public static double minus (double first, double second) {
+
+    public static double minus(double first, double second) {
         return first - second;
     }
-    public static double div (double first, double second) {
+
+    public static double div(double first, double second) {
         return first / second;
     }
 }
