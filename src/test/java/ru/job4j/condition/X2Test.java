0fb917b6fc49b68ab5fad277d35fd522007dcfb6
user <ruslano48@yandex.ru>
2024-05-25 23:03:20 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -2,15 +2,63 @@ package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class X2Test {
 
     @Test
-    void calc() {
+    void whenA10B0C0X2Then40() {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void main() {
+    void whenA1B1C1X1Then40() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
+
+    }
+
+    @Test
+    void whenABCXThen40() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA1BCXThen40() {
+        int a = 1;
+        int b = 0;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA1B1CXThen40() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
     }
 }
