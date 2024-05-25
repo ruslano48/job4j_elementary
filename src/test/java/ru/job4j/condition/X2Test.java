4b83f05cfd7cee6f33170ad251f02b36ba1d93c9
user <ruslano48@yandex.ru>
2024-05-25 21:39:19 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class X2Test {
+
+    @Test
+    void calc() {
+    }
+
+    @Test
+    void main() {
+    }
+}
