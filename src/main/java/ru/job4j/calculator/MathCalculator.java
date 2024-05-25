ba95b9b54ab05bffd7d8430354a28da0bb884e10
user <ruslano48@yandex.ru>
2024-05-25 20:57:18 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,7 +1,5 @@
 package ru.job4j.calculator;
 
-import ru.job4j.math.MathFunction;
-
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
@@ -9,9 +7,22 @@ public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                 + multiply(first, second);
+
+    }
+
+    public static double minusAndDiv(double first, double second) {
+        return   minus(second, first)
+                + div(second, first);
+    }
+    public static double sumMethod(double first, double second) {
+        return sumAndMultiply(first, second) + minusAndDiv(first, second);
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        double first = 10;
+        double second = 20;
+        System.out.println("Результат расчета sumAndMultiply равен: " + sumAndMultiply(first, second));
+        System.out.println("Результат расчета minusAndDiv равен: " + minusAndDiv(first, second));
+        System.out.println("Результат расчета sumMethod равен: " + sumMethod(first, second));
     }
 }
