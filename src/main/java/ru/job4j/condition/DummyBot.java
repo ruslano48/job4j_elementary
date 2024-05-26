d642facab0910e0de0e132bc3ec84a9581064431
user <ruslano48@yandex.ru>
2024-05-26 18:09:42 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String result = "I don't know. Please, ask another question.";
+        if ("Hi, Bot.".equals(question)) {
+            result = "Hi, SmartAss.";
+        } else if ("Bye.".equals(question)) {
+            result = "See you later.";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = DummyBot.answer("Hi, Bot.");
+        System.out.println(result); // Ожидается: "Hi, SmartAss."
+
+        result = DummyBot.answer("Bye.");
+        System.out.println(result); // Ожидается: "See you later."
+
+        result = DummyBot.answer("How are you?");
+        System.out.println(result); // Ожидается: "I don't know. Please, ask another question."
+    }
+}
+
