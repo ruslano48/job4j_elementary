67962af9401bcc156a58dd0291ae18f00b7a8529
user <ruslano48@yandex.ru>
2024-05-27 15:29:40 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+
+    public static void main(String[] agrs) {
+
+
+        int a = 10;
+        int b = 5;
+        int maxResult = max(a, b);
+        System.out.println("The maximum of " + a + " and " + b + " is " + maxResult);
+    }
+}
+
+
---
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MaxTest {
+    @Test
+    void whenMax1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax5To4Then5() {
+        int left = 5;
+        int right = 4;
+        int result = Max.max(left, right);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax10To20Then20() {
+        int left = 10;
+        int right = 20;
+        int result = Max.max(left, right);
+        int expected = 20;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1To1Then1() {
+        int left = 1;
+        int right = 1;
+        int result = Max.max(left, right);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+}
+
+
+
