1a885fcc6828eef111cb74166333e4abf88fa1cf
user <ruslano48@yandex.ru>
2024-05-30 06:57:28 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -8,36 +8,27 @@ public class SwitchWeek {
             case 1:
                 name = "Понедельник";
                 break;
-
             case 2:
                 name = "Вторник";
                 break;
-
             case 3:
                 name = "Среда";
                 break;
-
             case 4:
                 name = "Четверг";
                 break;
-
             case 5:
                 name = "Пятница";
                 break;
-
             case 6:
                 name = "Суббота";
                 break;
-
             case 7:
                 name = "Воскресение";
                 break;
-
             default:
-
                 name = "Ошибка";
                 break;
-
         }
         return name;
     }
