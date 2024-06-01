c5abbbfef82c5ed75af752100ee462db0ed881cc
user <ruslano48@yandex.ru>
2024-06-01 13:29:37 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/test/java/ru/job4j/loop/FactorialTest.java
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FactorialTest {
@@ -11,6 +12,7 @@ class FactorialTest {
         int output = Factorial.calculate(number);
         assertThat(output).isEqualTo(expected);
     }
+
     @Test
     void whenCalculateFactorialForZeroThenOne() {
         int expected = 1;
@@ -19,4 +21,3 @@ class FactorialTest {
         assertThat(output).isEqualTo(expected);
     }
 }
-
