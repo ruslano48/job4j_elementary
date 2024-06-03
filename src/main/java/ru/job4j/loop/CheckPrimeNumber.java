e98b1b6053e4ea56c48c28b8eddc4d20febe1ed1
user <ruslano48@yandex.ru>
2024-06-03 21:11:32 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
