6aa7e37156c859f451a71777d654f823a28448ea
user <ruslano48@yandex.ru>
2024-06-04 19:28:01 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("Размер массива равен:" + ages.length);
+        System.out.println("Размер массива равен:" + surnames.length);
+        System.out.println("Размер массива равен:" + prices.length);
+
+        String[] names = new String[5];
+        names[0] = "Иван Иванов";
+        names[1] = "Петр Петров";
+        names[2] = "Акакий Зелепукин";
+        names[3] = "Даздраперма Владленова";
+        names[4] = "Оюшминальда Первомаева";
+        System.out.println("Name:" + names[0]);
+        System.out.println("Name:" + names[1]);
+        System.out.println("Name:" + names[2]);
+        System.out.println("Name:" + names[3]);
+        System.out.println("Name:" + names[4]);
     }
 }
