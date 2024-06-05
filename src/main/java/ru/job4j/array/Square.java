712021d8985c79cdbc6fea98ab670d561091e11c
user <ruslano48@yandex.ru>
2024-06-05 12:18:32 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] result = new int[bound];
+        for (int i = 0; i < bound; i++) {
+            result[i] = (int) Math.pow(i, 2);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
