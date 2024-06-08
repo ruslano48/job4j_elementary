440196c4e362220d5fc5af78a53f5c47196e763e
user <ruslano48@yandex.ru>
2024-06-08 12:06:21 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -2,6 +2,14 @@ package ru.job4j.array;
 
 public class SwitchArray {
 
+    public static int[] swap(int[] array, int source, int destination) {
+        int temp = array[source];
+        array[source] = array[destination];
+        array[destination] = temp;
+
+        return array;
+    }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
@@ -17,3 +25,4 @@ public class SwitchArray {
         }
     }
 }
+
--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SwitchArrayTest {
@@ -33,6 +34,16 @@ class SwitchArrayTest {
         int[] input = {1};
         int[] result = SwitchArray.swapBorder(input);
         int[] expected = {1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
         assertThat(result).containsExactly(expected);
     }
 }
