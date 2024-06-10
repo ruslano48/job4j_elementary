21915675923919979ba7c818c06ee055adf1e0ef
user <ruslano48@yandex.ru>
2024-06-10 15:28:16 +0300

    7. Расстояние между точками в системе координат [#188 #497034]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] prefix) {
+        if (prefix.length > word.length)
+        {
+            return false;
+        }
+
+            for (int i = 0; i < prefix.length; i++) {
+                if (word[i] != prefix[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
