79b65d8fc5dd0843984701b6a00b7ce16aae4bab
user <ruslano48@yandex.ru>
2024-10-14 11:36:21 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -6,20 +6,43 @@ import static java.lang.Math.sqrt;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println("Point " + this.x + ", " + this.y + ", " + this.z + ".");
     }
 
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double distance = a.distance(b);
-        System.out.println(distance);
+        System.out.println("2D distance: " + distance);
+
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(0, 1, 1);
+        double distance3d = p1.distance3d(p2);
+        System.out.println("3D distance: " + distance3d);
+
+        a.info();
+        p1.info();
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class PointTest {
@@ -35,5 +36,29 @@ public class PointTest {
         Point b = new Point(3, 4);
         double result = a.distance(b);
         assertEquals(5.0, result, 0.001);
+    }
+
+    @Test
+    public void whenDistance3dIsZero() {
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(0, 0, 0);
+        double result = p1.distance3d(p2);
+        assertEquals(0, result, 0.01);
+    }
+
+    @Test
+    public void whenDistance3dIsOne() {
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(0, 1, 0);
+        double result = p1.distance3d(p2);
+        assertEquals(1, result, 0.01);
+    }
+
+    @Test
+    public void whenDistance3dIsSqrt3() {
+        Point p1 = new Point(0, 0, 0);
+        Point p2 = new Point(1, 1, 1);
+        double result = p1.distance3d(p2);
+        assertEquals(Math.sqrt(3), result, 0.01);
     }
 }
